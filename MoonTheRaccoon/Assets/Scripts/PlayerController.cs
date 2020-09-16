e9c5999f743c212d4ca973b26627b220c96965c4
sharnold52 <sharnold52@gmail.com>
2020-09-16 17:38:00 -0400

    Sean Arnold - Fixed hiding spot in Player Controller

--- MoonTheRaccoon/Assets/Scripts/PlayerController.cs
+++ MoonTheRaccoon/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rBody2d;
     AudioSource audioSource;
     Animator animator;
+    Collider2D playerCol;
 
     // Member Variables
     Vector2 movement;
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
         rBody2d = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
+        playerCol = GetComponent<Collider2D>();
     }
 
     // Handle Input
@@ -122,6 +124,7 @@ public class PlayerController : MonoBehaviour
     {
         // hide sprite
         isHidden = true;
+        playerCol.enabled = false;
         gameObject.GetComponent<SpriteRenderer>().enabled = false;
         gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
 
@@ -133,6 +136,7 @@ public class PlayerController : MonoBehaviour
     {
         // hide sprite
         isHidden = false;
+        playerCol.enabled = true;
         gameObject.GetComponent<SpriteRenderer>().enabled = true;
         gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
     }
